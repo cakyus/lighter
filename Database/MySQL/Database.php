382fdb1bf1a0225e02b424d28ed46e49ba02ed8f
cakyus <cakyus@gmail.com>
2013-09-03 19:32:21 +0700

    add prototype for database access

---
+++ Database/MySQL/Database.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ **/
+
+namespace Lighter\Database\MySQL;
+
+class Database implements \Lighter\Database\Interfaces\Database {
+	
+	public function open() {
+		
+	}
+	
+	public function close() {
+		
+	}
+	
+	public function exec($sql) {
+		
+	}
+	
+	public function query($sql) {
+		
+	}
+	
+	public function escape($string) {
+		
+	}
+	
+	public function insertId() {
+		
+	}
+	
+	public function beginTransaction() {
+		
+	}
+	
+	public function commit() {
+		
+	}
+	
+	public function rollback() {
+		
+	}
+}
